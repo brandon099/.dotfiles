da5e83f5a9ecf33867771eb22fa3f5df5d4400e1
Brandon Clifford <brandon.clifford@vivint.com>
2015-03-08 20:28:01 -0600

    Adding Xresources and replaced conky bar with plain bar

--- .makesym.sh
+++ .makesym.sh
@@ -4,7 +4,7 @@ dir=~/.dotfiles
 olddir=~/.dotfiles_old
 
 # List of files/folders to symlink in homedir
-files="bashrc vimrc vim zshrc oh-my-zsh dircolors gitconfig scripts"
+files="bashrc vimrc vim zshrc oh-my-zsh dircolors gitconfig scripts Xresources config"
 
 # Create .dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles..."
